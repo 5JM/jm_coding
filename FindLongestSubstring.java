2e98fd714157956a8b423fd48fd249854c1570b6
5JM <toddlf0614@naver.com>
2021-02-02 17:24:11 +0900

    예제추가

--- FindLongestSubstring.java
+++ FindLongestSubstring.java
@@ -1,9 +1,9 @@
 public class FindLongestSubstring {
     public static void main(String args[]){
-        System.out.println(lengthOfLongestSubstring("dvdf"));
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
         return;
     }
-    //abcabcvv
+    //abcabcqwerty
     static public int lengthOfLongestSubstring(String s) {
         char[] chars = s.toCharArray();
         int start = 0;
@@ -11,17 +11,14 @@ public class FindLongestSubstring {
         for (int i = 0; i < s.length(); i++) {
             for (int j = start; j < i; j++) {
                 if (chars[i] == chars[j]) {
-                    if (i - start > max) {
-                        max = i - start;
-                    }
+                    if (i - start > max) { max = i - start; }
                     start = j + 1;
                     break;
                 }
             }
         }
-        if(s.length() - start > max) {
-            max = s.length() - start;
-        }
+        //문자열 뒷부분이 반복되는 부분이 없을때 ex. abcabcqwerty
+        if(s.length() - start > max) { max = s.length() - start; }
         return max;
     }
 }
